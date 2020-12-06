1aa0b55e13200a3640e33763ebfc171a81d662c5
RamiB1234 <rbisthebest@hotmail.com>
2020-12-06 16:55:43 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import DataTable from 'react-data-table-component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const data = [{ id: 1, type: 'tempreture', value: '23.2', alert: 'false' },
+{ id: 1, type: 'tempreture', value: '23.2', alert: 'false' },
+{ id: 1, type: 'tempreture', value: '23.2', alert: 'false' },
+{ id: 1, type: 'tempreture', value: '23.2', alert: 'false' },
+{ id: 1, type: 'tempreture', value: '23.2', alert: 'false' },];
+const columns = [
+  {
+    name: 'ID',
+    selector: 'id',
+  },
+  {
+    name: 'Type',
+    selector: 'type',
+  },
+  {
+    name: 'Value',
+    selector: 'value',
+  },
+  {
+    name: 'Alert',
+    selector: 'alert',
+  },
+];
+
+class App extends React.Component {
+  state = {
+
+  };
+  render(){
+    return (
+      <DataTable
+      title="Sensor Reading Data"
+      columns={columns}
+      data={data}
+      />
+    );
+  }
 }
 
 export default App;
